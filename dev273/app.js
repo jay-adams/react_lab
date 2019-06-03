fe1e82e0b98f4e46c09b30dfe398f806f6d35309
Joseph Adams <jadams41@nyc.rr.com>
2019-06-03 16:31:40 -0400

    add dev273 TypeScript 2

---
+++ dev273/app.js
@@ -0,0 +1,9 @@
+function countdown(initial, final, interval) {
+    if (final === void 0) { final = 0; }
+    if (interval === void 0) { interval = 1; }
+    var current = initial;
+    while (current > final) {
+        console.log(current);
+        current -= interval;
+    }
+}
---
+++ dev273/app.ts
@@ -0,0 +1,12 @@
+function countdown({
+    initial, 
+    final: final = 0, 
+    interval: interval = 1,
+    initial: current
+}){
+ 
+    while (current > final) {
+        console.log(current);
+        current -= interval;
+    }
+}
