7a2e857341bc054b1be9aa69989a18351ce99bf9
Joseph Adams <jadams41@nyc.rr.com>
2019-06-30 11:57:08 -0400

    Add react-debugging project

---
+++ dev273/TodoListComponent.d.ts
@@ -0,0 +1,7 @@
+declare var $: any;
+declare class TodoListComponent {
+    private $el;
+    constructor(el: HTMLElement);
+    render(todos: any): void;
+    private renderTodo;
+}
--- dev273/app.js
+++
@@ -1,9 +0,0 @@
-function countdown(initial, final, interval) {
-    if (final === void 0) { final = 0; }
-    if (interval === void 0) { interval = 1; }
-    var current = initial;
-    while (current > final) {
-        console.log(current);
-        current -= interval;
-    }
-}
